e86ec78306214c859e780d04688a6fddd46d1c12
Peter Pauk <petkopauk@gmail.com>
2023-05-08 08:43:55 +0200

    about us page almost complete

--- js/index.js
+++ js/index.js
@@ -6,16 +6,8 @@ function changeBg() {
 
   if (scrollValue > 1) {
     navbar.classList.add("bgColor");
-    btn.classList.add("white_button");
-    btn.classList.remove("btn1");
-    night.classList.add("black");
-    night.classList.remove("white");
   } else {
     navbar.classList.remove("bgColor");
-    btn.classList.remove("white_button");
-    btn.classList.add("btn1");
-    night.classList.remove("black");
-    night.classList.add("white");
   }
 }
 window.addEventListener("scroll", changeBg);
